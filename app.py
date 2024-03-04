50b69213a5d0db4ee7d818656a2135e75774c33a
Emily Walker <ewalker415@gmail.com>
2024-03-04 11:51:25 -0800

    created cupcake model and get routes

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
-"""Flask app for Cupcakes"""
+import os
+
+from flask import Flask, jsonify
+# from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", "postgresql:///cupcakes")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# toolbar = DebugToolbarExtension(app)
+
+@app.get("/api/cupcakes")
+def get_cupcakes():
+    """ Get data about all cupcakes """
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes = serialized)
+
+@app.get("/api/cupcakes/<int:cupcake-id>")
+def get_cupcake_details(cupcake_id):
+    """ Get data about single cupcake """
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake = serialized)
