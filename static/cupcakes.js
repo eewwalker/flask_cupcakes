3ccf4eec84f42058e3aa2f727b07de79e8ee425f
Emily Walker <ewalker415@gmail.com>
2024-03-04 17:59:05 -0800

    added docstrings

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -2,13 +2,17 @@
 
 const $cupcakeList = $("#cupcake-list");
 
+/** getCupcakes request to get all cupcakes in db  */
+
 async function getCupcakes() {
 
   const resp = await fetch('/api/cupcakes');
   const data = await resp.json();
-  // console.log(data.cupcakes[0]);
+
   return data.cupcakes;
 }
+
+/** htmlMarkup takes obj keys and creates html markup with values */
 
 function htmlMarkup({ flavor, rating, size, image_url }) {
 
@@ -16,22 +20,33 @@ function htmlMarkup({ flavor, rating, size, image_url }) {
     `<div>${flavor}</div>
       <div>${rating}</div>
       <div>${size}</div>
-      <div><img src="${image_url}" style="height: 150px"></img></div>`
+      <div>
+        <img src="${image_url}" style="height: 150px"></img>
+      </div>`
   );
 }
 
+/** displayCupcakes takes cupcakes array and displays on page  */
 function displayCupcakes(cupcakes) {
+
   for (const cupcake of cupcakes) {
+
     const $liCupcake = $("<li>").html(htmlMarkup(cupcake));
     $cupcakeList.append($liCupcake);
+
   }
+
 }
 
+/** start gets cupcakes from API and displays on page */
 async function start() {
+
   const cupCakeData = await getCupcakes();
   displayCupcakes(cupCakeData);
+
 }
 
+/** addNewCupcake makes request to add newCupCake to server  */
 async function addNewCupcake(flavor, size, rating, image_url) {
 
   const cupcakeData = JSON.stringify(
@@ -48,12 +63,16 @@ async function addNewCupcake(flavor, size, rating, image_url) {
     headers: {
       "Content-Type": "application/json"
     },
+
     body: cupcakeData
+
   });
 
   return await response.json();
 
 };
+
+/** handleSubmit event handler; gets form value and displays markup on page  */
 
 async function handleSubmit(evt) {
   evt.preventDefault();
@@ -66,6 +85,7 @@ async function handleSubmit(evt) {
   const newCupcake = await addNewCupcake(flavor, size, rating, image_url);
   const $newLI = htmlMarkup(newCupcake.cupcake);
   $cupcakeList.append($newLI);
+
 }
 
 
