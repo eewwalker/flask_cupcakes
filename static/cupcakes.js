dd504f1ce3b59356dd981b7ffaeac46518d94772
Emily Walker <ewalker415@gmail.com>
2024-03-04 16:33:32 -0800

    added cupcakes.js

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -1,32 +1,55 @@
 "use strict";
 
-const BASE_URL = "https://localhost:5001"
-const $cupcakeList = $("#cupcake-list")
+const $cupcakeList = $("#cupcake-list");
 
-async function addNewCupcake(evt){
-  evt.preventDefault()
+async function getCupcakes() {
 
-  const flavor = $("#flavor").val()
-  const size = $("#size").val()
-  const rating = $("#rating").val()
-  const image_url = $("#image_url").val()
+  const resp = await fetch('/api/cupcakes');
+  const data = await resp.json();
+  // console.log(data.cupcakes[0]);
+  return data.cupcakes;
+}
 
-  cupcake_data = JSON.stringify(
-    {
-      "flavor": flavor,
-      "size": size,
-      "rating": rating,
-      "image_url": image_url
-    }
-  )
+function displayCupcakes(cupcakes) {
+  for (const cupcake of cupcakes) {
+    console.log(cupcake);
+    const $liCupcake = $("<li>").html(cupcake.flavor, cupcake.rating, cupcake.size, cupcake.image_url);
+    $cupcakeList.append($liCupcake);
+  }
+}
 
-  await fetch(`${BASE_URL}/api/cupcakes`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: cupcake_data
-  })
-};
 
-$("#new-cupcake-form").on("submit", await addNewCupcake);
+function start() {
+  const cupCakeData = getCupcakes();
+  displayCupcakes(cupCakeData);
+}
+
+// async function addNewCupcake(evt) {
+//   evt.preventDefault();
+
+//   const flavor = $("#flavor").val();
+//   const size = $("#size").val();
+//   const rating = $("#rating").val();
+//   const image_url = $("#image_url").val();
+
+//   cupcake_data = JSON.stringify(
+//     {
+//       "flavor": flavor,
+//       "size": size,
+//       "rating": rating,
+//       "image_url": image_url
+//     }
+//   );
+
+//   await fetch(`${BASE_URL}/api/cupcakes`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: cupcake_data
+//   });
+// };
+
+// $("#new-cupcake-form").on("submit", await addNewCupcake);
+
+start();
